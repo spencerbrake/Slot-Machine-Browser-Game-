35dacca320148da503dd84c946d06ab4d92146f1
spencer brake <spencerbrake98@gmail.com>
2021-05-31 11:24:43 -0700

    added html elements

---
+++ docs/psuedocode.md
@@ -0,0 +1,14 @@
+1. When user opens game they see a slot machine 
+2. Slot machine has a lever, 3 rows with symbols in random order, a credit amount listed, bet entry field set to blank(waiting for user input) 
+3. a user should be able to enter a bet amount <= credit amount 
+4. once a bet amount is enetered, then user should be able to click the lever to spin the wheels
+5. spin will generate random symbols on each click
+6. wheels should be checked for any winnning patterns on each spin
+7. winning patterns should be hightlighted on the screen
+8. "Winner" message should display with winning patters.
+9. "Try again" message if no winning patterns 
+10. credit amount should be adjusted based on if player wins 
+11. hightlighted winning patterns should resets on each new spin 
+12. bet amount should reset back to blank after each spin 
+13. A user cant spin untill the bet amount is entered 
+14. game over message if credit = 0
--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+//  When user opens game they see a slot machine 
+
+//  Slot machine has a lever, 3 rows with symbols in random order, 
+//  a credit amount listed, bet entry field set to blank(waiting for user input) 
+
+//  a user should be able to enter a bet amount <= credit amount 
+
+// once a bet amount is enetered, then user should be able to click the lever to spin the wheels
+
+//  spin will generate random symbols on each click
+
+//  wheels should be checked for any winnning patterns on each spin
+
+//  winning patterns should be hightlighted on the screen
+
+//  "Winner" message should display with winning patters.
+
+// "Try again" message if no winning patterns 
+
+//  credit amount should be adjusted based on if player wins 
+
+//  hightlighted winning patterns should resets on each new spin 
+
+//  bet amount should reset back to blank after each spin 
+
+//  A user cant spin untill the bet amount is entered 
+
+//  game over message if credit = 0
+
+
+
+
+
 /*----- constants -----*/
 
 
