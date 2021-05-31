9a637bb837fb2b450fecb4b5b1d7728719854376
spencer brake <spencerbrake98@gmail.com>
2021-05-31 13:49:34 -0700

    added images

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-//  When user opens game they see a virtual slot machine 
-
-//  Slot machine has 6 boxes in a row with symbols in random order, 
-//  a credit amount listed, bet amount set to blank(waiting for user to click bet) 
-
-
-// once bet button is clicked, 20 will be added to bet amount, 
-//then user should be able to click spin button to start the game.
-
-//  spin will generate random symbols on each click
-
-//  wheels should be checked for any matching symbols on each spin
-
-//  matching symblos should be hightlighted on the screen
-
-//  "Winner" message should display if user wins.
-
-// "Try again" message if user loses & credit amout > 0
-
-//  credit amount should be adjusted based on if player wins 
-
-
-//  bet amount should reset back to blank after each spin 
-
-//  A user cant spin untill the bet is clicked at least once. 
-// bet button can be clicked until full credit amount is gone.
-//winning doubles the bet amount entered
-
-
-//  'game over' message if credit = 0
 
 
 
