c5ba2d0029494a87c00d5a6b05e4decd321e2488
spencer brake <spencerbrake98@gmail.com>
2021-06-01 15:06:48 -0700

    constants & variables

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-
-
-
-
-
 /*----- constants -----*/
 
-
+const icons = {
+    seven: 'imgs/7-symbol.jpg',
+    bar: 'imgs/BAR.jpg',
+    bell: 'imgs/BELL.jpg',
+    cherry: 'imgs/cherry.jpg',
+    coin: 'imgs/coin.jpg',
+    grape: 'imgs/grapes.jpg',
+    lemon: 'imgs/lemon.jpg',
+    plum: 'imgs/plum.jpg',
+}
 
 
 
 
 /*----- app's state (variables) -----*/
+
+let credit;
+let bet;
+let slots;
+let winner;
 
 
 
